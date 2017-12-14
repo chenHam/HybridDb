c2eff7a5c9780365c6adea8e16a94a4e81c726ce
ChenHam <Chen@DESKTOP-8HEVHG2>
2017-12-14 19:24:25 +0200

    1. add powerSetFinder function to demo 2. add timer

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
 import pymysql;
 import time;
-
+import powerSetFinder as psf;
 conn = pymysql.connect(host='193.106.55.134', port=3306, user='root', password='root', db='northwind')
 
 cursor = conn.cursor()
@@ -10,16 +10,19 @@ cursor.execute("SHOW TABLES")
 for (table_name,) in cursor:
         print(table_name)
         cursor.execute('SELECT * FROM northwind.'+table_name+';')
-        columns = cursor.description
+        columns = [i[0] for i in cursor.description]
+        powerset = psf.listToPowerset(columns)
 
-        for (a,) in arr:
-            query = 'SELECT' + a[0] + 'FROM northwind.' + table_name + ';'
+        for (ps) in powerset:
+            cols = ', '.join(ps)
+            query = 'SELECT ' + cols + ' FROM northwind.' + table_name + ';'
 
             t0 = time.time()
             cursor.execute(query)
             t1 = time.time()
 
             total = t1 - t0
+            print('')
             #OrtalCode
 
 print ('')
