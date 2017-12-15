656dfa95ac488690a82079e61f00f8a8ed966c3b
barbrownshtein <bar810@gmail.com>
2017-12-15 12:23:11 +0200

    db connections closed simplified queries

--- demo.py
+++ demo.py
@@ -1,28 +1,96 @@
 import pymysql;
 import time;
 import powerSetFinder as psf;
-conn = pymysql.connect(host='193.106.55.134', port=3306, user='root', password='root', db='northwind')
 
+def openConnection():
+    conn = pymysql.connect(host='193.106.55.134', port=3306, user='root', password='root', db='northwind')
+
+    return conn
+
+def closeConnection(con):
+    con.close()
+
+def getTableNames():
+    conn = openConnection()
+    cursor = conn.cursor()
+    cursor.execute("select table_name from information_schema.tables where table_schema = 'northwind'")
+    tableNames = cursor.fetchall()
+    closeConnection(conn)
+
+    return tableNames
+
+def getTableColumnNames(tableName):
+
+    conn = openConnection()
+    cursor = conn.cursor()
+    query = "select column_name from information_schema.columns where table_name = '" + tableName + "'"
+    cursor.execute(query)
+    columns = cursor.fetchall()
+    closeConnection(conn)
+
+    return columns
+
+def getColumnsPowerSet(columns):
+    return psf.listToPowerset(columns)
+
+def getQueryTime(query):
+    con = openConnection()
+    cursor = con.cursor()
+    t0 = time.time()
+    cursor.execute(query)
+    t1 = time.time()
+    closeConnection(con)
+
+    total = t1 - t0
+    return total
+
+
+conn = openConnection()
 cursor = conn.cursor()
+cursor.execute("select table_name from information_schema.tables where table_schema = 'northwind'")
+tableNames = cursor.fetchall()
+closeConnection(conn)
 
-cursor.execute("SHOW TABLES")
+for tableName in tableNames:
+    tableNameStr = tableName[0]
+    print(tableNameStr)
 
-for (table_name,) in cursor:
-        print(table_name)
-        cursor.execute('SELECT * FROM northwind.'+table_name+';')
-        columns = [i[0] for i in cursor.description]
-        powerset = psf.listToPowerset(columns)
+    conn = openConnection()
+    cursor = conn.cursor()
+    query = "select column_name from information_schema.columns where table_name = '" + tableNameStr + "'"
+    cursor.execute(query)
+    closeConnection(conn)
 
-        for (ps) in powerset:
-            cols = ', '.join(ps)
-            query = 'SELECT ' + cols + ' FROM northwind.' + table_name + ';'
+    tableColumns = [i[0] for i in cursor.fetchall()]
+    columnsPowerset = psf.listToPowerset(tableColumns)
 
+    for (powerset) in columnsPowerset:
+        try:
+            colsJoined = ', '.join(powerset)
+            print("cols: " + colsJoined)
+
+            query = 'SELECT ' + colsJoined + ' FROM northwind.' + tableNameStr + ';'
+            print('query: ' + query)
+
+            con = openConnection()
+            cursor = con.cursor()
             t0 = time.time()
             cursor.execute(query)
             t1 = time.time()
+            closeConnection(con)
 
             total = t1 - t0
-            print('')
-            #OrtalCode
+            print("done with cols, time: " + str(total))
+        except Exception as e:
+            print("exception: " + e)
 
+            print('')
+            # OrtalCode
+
+    print("done with current table")
+print ("done")
 print ('')
+
+
+
+
