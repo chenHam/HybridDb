676e0fd284b8d644746883a0c6414c4929f503ca
OrtalYona <ortalyona94@gmail.com>
2018-02-07 19:05:43 +0200

    Query to data frame

---
+++ QueryToDataFrame.py
@@ -0,0 +1,31 @@
+import pymysql
+import pandas as pd
+
+
+conn = pymysql.connect(host="193.106.55.134", port=3306, user='root', password='root', db='winecellar')
+
+cursor = conn.cursor()
+cursor.execute("SELECT EVENT_ID, \
+                        TRUNCATE(TIMER_WAIT/1000000000000,6) as Duration,\
+                        SQL_TEXT\
+                FROM performance_schema.events_statements_history_long\
+                WHERE SQL_TEXT like '%wine%' "
+               "        and SQL_TEXT not like '%SQL_TEXT%'"
+               "        and SQL_TEXT like '%SELECT%'"
+                )
+results = cursor.fetchall()
+list = []
+for r in results:
+    print(r)
+    list.append(r)
+
+cols = ['RunTime', 'Query']
+df = pd.DataFrame(list, columns=['x','RunTime','Query'])
+del df['x']
+
+df.to_csv("DataFrame.csv")
+
+
+conn.close()
+
+
