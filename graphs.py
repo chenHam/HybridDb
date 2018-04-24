3d60e9a24d8aaf1f1875780ee8dee96b2bef0ce5
OrtalYona <ortalyona94@gmail.com>
2018-04-24 21:05:18 +0300

    graphs

---
+++ graphs.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+#data
+df = pd.read_csv('mainCsv.csv')
+dfA = pd.read_csv('mainCsvA.csv')
+data = df[['RunningTime','SumOfRunning']].plot('RunningTime','SumOfRunning',marker='o',color='skyblue')
+dataA = dfA[['RunningTime','SumOfRunning']].plot('RunningTime','SumOfRunning',marker='o',color='olive')
+# dataA = dfA[['RunningTime','SumOfRunning']].plot('RunningTime','SumOfRunning',ax=data,marker='', color='olive')
+plt.xlabel("Time from beginning")
+plt.ylabel("Total time completed")
+plt.show()
+
