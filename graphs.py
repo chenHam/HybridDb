c7319cf566282bce01f7f7024b35646d7c0aa4a5
OrtalYona <ortalyona94@gmail.com>
2018-04-25 19:42:16 +0300

    graph

--- graphs.py
+++ graphs.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv('mainCsv.csv')
-dfA = pd.read_csv('mainCsvA.csv')
-data = df[['RunningTime','SumOfRunning']].plot('RunningTime','SumOfRunning',marker='o',color='skyblue')
-dataA = dfA[['RunningTime','SumOfRunning']].plot('RunningTime','SumOfRunning',marker='o',color='olive')
-# dataA = dfA[['RunningTime','SumOfRunning']].plot('RunningTime','SumOfRunning',ax=data,marker='', color='olive')
+df = pd.read_csv('mainCsv-fat-1.csv')
+dfA = pd.read_csv('mainCsv-thin-1.csv')
+data = df[['RunningTime','SumOfRunning']].plot('RunningTime','SumOfRunning',label="Fat",marker='o',color='skyblue')
+# dataA = dfA[['RunningTime','SumOfRunning']].plot('RunningTime','SumOfRunning',marker='o',color='olive')
+dataA = dfA[['RunningTime','SumOfRunning']].plot('RunningTime','SumOfRunning',ax=data,label="Thin",marker='', color='olive')
 plt.xlabel("Time from beginning")
 plt.ylabel("Total time completed")
 plt.show()
