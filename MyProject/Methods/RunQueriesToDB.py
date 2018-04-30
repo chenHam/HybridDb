0a3d0f011536a7480f5a16941ec3d0efe3cbe36a
refaela <refaela@playtika.com>
2018-05-01 00:30:28 +0300

    added new run fat csv

--- MyProject/Methods/RunQueriesToDB.py
+++ MyProject/Methods/RunQueriesToDB.py
@@ -1,108 +1,108 @@
-import random
-import sys
-from multiprocessing import Pool
-from pip._vendor import requests
-import time
-import json
-import MyProject.Methods.GetLearningModifyTime as tfm
-import datetime
-from datetime import datetime
-import MyProject.FilesAndInputs.winesArrayAsJson as wines
-from test import ScheduledTask
-
-def insertQuery(wine,port):
-    r = requests.post("http://193.106.55.134:"+port+"/wines", data=wine)
-    print("insert return: ",r.status_code, r.reason," port: ",port)
-
-def getQuery(wine,port):# small 3002. big 3001
-    r = requests.post("http://193.106.55.134:"+port+"/getwines/",data=wine, timeout = 999999)
-    print("get return: ",r.status_code, r.reason," port: ",port)
-
-def updateQuery(wine1,wine2,port):
-    headers = {'Content-Type': 'application/json'}
-    data = [wine1, wine2]
-    r = requests.put(url="http://193.106.55.134:"+port+"/wines",  json=data, headers = headers)
-    print("update return: ",r.status_code, r.reason," port: ",port)
-
-
-def runUpdateQuery(distribution,windowTime,port):
-    timeInSec=windowTime*60
-    queryInterval=timeInSec/distribution
-    for i in range(0, distribution):
-        time.sleep(queryInterval)
-        numOfWine1 = random.randint(0, 23)
-        numOfWine2 = random.randint(0, 23)
-        print("Run update query number  " + str(i)+"...")
-        updateQuery(wines.wines[numOfWine1],wines.wines[numOfWine2],port)
-
-def runGetQuery(distribution,windowTime,queryWineSize,port):
-  timeInSec = windowTime * 60
-  queryInterval = timeInSec / distribution
-
-  sc = ScheduledTask(distribution)
-
-  sc.runTask(getQuery, queryInterval, args = [wines.wines2[queryWineSize],port])
-
-  while sc.isDone() == False:
-      time.sleep(1)
-
-  # for i in range(0, distribution):
-  #   time.sleep(queryInterval)
-  #   print("Run get-big query number  "+str(i)+"...")
-  #   getQuery(wines.wines2[queryWineSize],port)
-
-
-
-
-
-def runInsertQuery(distribution,windowTime,port):
-  timeInSec = windowTime * 60
-  queryInterval = timeInSec / distribution
-  for i in range(0, distribution):
-    time.sleep(queryInterval)
-    numOfWine1 = random.randint(0, 23)
-    print("Run insert query number  "+str(i)+"...")
-    insertQuery(wines.wines[numOfWine1],port)
-
-def runFunc(params):
-    startTime = datetime.now()
-    if(len(sys.argv)<2):
-        print("Insert arguments !!!!")
-        exit(1)
-    dists=json.loads(sys.argv[1])
-    actionIterations=len(dists)
-    windowTime = int(sys.argv[2])
-    numOfIterations=int(sys.argv[3])
-    for i in range(0,numOfIterations):
-        print(params[0]," iteration: ",i)
-        for i in range(0, actionIterations):
-          nowtime=datetime.now()
-          if(sys.argv[4]=="1"):
-            port=initialPort((nowtime-startTime).total_seconds())
-          else:
-              port=params[1]
-          if (params[0] == "getBig"):
-            runGetQuery(dists[i][0],windowTime,1,port)
-          elif (params[0] == "getSmall"):
-            runGetQuery(dists[i][1], windowTime,0,port)
-          elif (params[0] == "insert"):
-            runInsertQuery(dists[i][2],windowTime,port)
-          elif (params[0] == "update"):
-            runUpdateQuery(dists[i][3],windowTime,port)
-          else:
-            print("Unknown command !!")
-
-def initialPort(time):
-    res=tfm.getPrediction(time)
-    if(res=="fat"):
-      print("initial port: 3002")
-      return "3002"
-    else:
-        print("initial port: 3001")
-        return "3001"
-
-def Run(thePort):
-    print("----------Start to Run all the queries----------")
-    p = Pool(2)
-    p.map(runFunc, [["getBig",thePort],["getSmall",thePort]]) # OPTIONAL TO ADD :: ,"insert","update"
-    print("----------Finish to Run all the queries----------")
+# import random
+# import sys
+# from multiprocessing import Pool
+# from pip._vendor import requests
+# import time
+# import json
+# import MyProject.Methods.GetLearningModifyTime as tfm
+# import datetime
+# from datetime import datetime
+# import MyProject.FilesAndInputs.winesArrayAsJson as wines
+# #from test import ScheduledTask
+#
+# def insertQuery(wine,port):
+#     r = requests.post("http://193.106.55.134:"+port+"/wines", data=wine)
+#     print("insert return: ",r.status_code, r.reason," port: ",port)
+#
+# def getQuery(wine,port):# small 3002. big 3001
+#     r = requests.post("http://193.106.55.134:"+port+"/getwines/",data=wine, timeout = 999999)
+#     print("get return: ",r.status_code, r.reason," port: ",port)
+#
+# def updateQuery(wine1,wine2,port):
+#     headers = {'Content-Type': 'application/json'}
+#     data = [wine1, wine2]
+#     r = requests.put(url="http://193.106.55.134:"+port+"/wines",  json=data, headers = headers)
+#     print("update return: ",r.status_code, r.reason," port: ",port)
+#
+#
+# def runUpdateQuery(distribution,windowTime,port):
+#     timeInSec=windowTime*60
+#     queryInterval=timeInSec/distribution
+#     for i in range(0, distribution):
+#         time.sleep(queryInterval)
+#         numOfWine1 = random.randint(0, 23)
+#         numOfWine2 = random.randint(0, 23)
+#         print("Run update query number  " + str(i)+"...")
+#         updateQuery(wines.wines[numOfWine1],wines.wines[numOfWine2],port)
+#
+# def runGetQuery(distribution,windowTime,queryWineSize,port):
+#   timeInSec = windowTime * 60
+#   queryInterval = timeInSec / distribution
+#
+#   sc = ScheduledTask(distribution)
+#
+#   sc.runTask(getQuery, queryInterval, args = [wines.wines2[queryWineSize],port])
+#
+#   while sc.isDone() == False:
+#       time.sleep(1)
+#
+#   # for i in range(0, distribution):
+#   #   time.sleep(queryInterval)
+#   #   print("Run get-big query number  "+str(i)+"...")
+#   #   getQuery(wines.wines2[queryWineSize],port)
+#
+#
+#
+#
+#
+# def runInsertQuery(distribution,windowTime,port):
+#   timeInSec = windowTime * 60
+#   queryInterval = timeInSec / distribution
+#   for i in range(0, distribution):
+#     time.sleep(queryInterval)
+#     numOfWine1 = random.randint(0, 23)
+#     print("Run insert query number  "+str(i)+"...")
+#     insertQuery(wines.wines[numOfWine1],port)
+#
+# def runFunc(params):
+#     startTime = datetime.now()
+#     if(len(sys.argv)<2):
+#         print("Insert arguments !!!!")
+#         exit(1)
+#     dists=json.loads(sys.argv[1])
+#     actionIterations=len(dists)
+#     windowTime = int(sys.argv[2])
+#     numOfIterations=int(sys.argv[3])
+#     for i in range(0,numOfIterations):
+#         print(params[0]," iteration: ",i)
+#         for i in range(0, actionIterations):
+#           nowtime=datetime.now()
+#           if(sys.argv[4]=="1"):
+#             port=initialPort((nowtime-startTime).total_seconds())
+#           else:
+#               port=params[1]
+#           if (params[0] == "getBig"):
+#             runGetQuery(dists[i][0],windowTime,1,port)
+#           elif (params[0] == "getSmall"):
+#             runGetQuery(dists[i][1], windowTime,0,port)
+#           elif (params[0] == "insert"):
+#             runInsertQuery(dists[i][2],windowTime,port)
+#           elif (params[0] == "update"):
+#             runUpdateQuery(dists[i][3],windowTime,port)
+#           else:
+#             print("Unknown command !!")
+#
+# def initialPort(time):
+#     res=tfm.getPrediction(time)
+#     if(res=="fat"):
+#       print("initial port: 3002")
+#       return "3002"
+#     else:
+#         print("initial port: 3001")
+#         return "3001"
+#
+# def Run(thePort):
+#     print("----------Start to Run all the queries----------")
+#     p = Pool(2)
+#     p.map(runFunc, [["getBig",thePort],["getSmall",thePort]]) # OPTIONAL TO ADD :: ,"insert","update"
+#     print("----------Finish to Run all the queries----------")
--- test.py
+++ test.py
@@ -1,89 +1,89 @@
-import schedule
-import time
-import datetime
-
-# from apscheduler.events import EVENT_ALL, EVENT_JOB_EXECUTED
+# import schedule
+# import time
+# import datetime
+#
+# # from apscheduler.events import EVENT_ALL, EVENT_JOB_EXECUTED
+# #
+# #
+# from apscheduler.events import EVENT_JOB_EXECUTED
 #
 #
-from apscheduler.events import EVENT_JOB_EXECUTED
-
-
-class Counter():
-    count = 0
-
-    def inc(self):
-        self.count = self.count + 1
-
-    def get(self):
-        return self.count
-
+# class Counter():
+#     count = 0
 #
-# def job():
+#     def inc(self):
+#         self.count = self.count + 1
 #
-#     print("I'm working...", datetime.datetime.now())
-#     print(len(schedule.jobs))
-#     time.sleep(5)
-#     print("done working...", datetime.datetime.now())
+#     def get(self):
+#         return self.count
 #
-# max = 3
+# #
+# # def job():
+# #
+# #     print("I'm working...", datetime.datetime.now())
+# #     print(len(schedule.jobs))
+# #     time.sleep(5)
+# #     print("done working...", datetime.datetime.now())
+# #
+# # max = 3
+# #
+# #
+# # from apscheduler.schedulers.background import BackgroundScheduler
+# #
+# #
+# #
+# # def some_job(counter):
+# #     counter.inc()
+# #     print('start', datetime.datetime.now())
+# #     time.sleep(5)
+# #     print('done', datetime.datetime.now())
+# #
+# #
+# # # seconds can be replaced with minutes, hours, or days
+# #
+# #
+# # counter = Counter()
+# #
+# #
+# # while counter.get() < max:
+# #     time.sleep(1)
 #
 #
+# #sched.shutdown()
 # from apscheduler.schedulers.background import BackgroundScheduler
 #
 #
+# class ScheduledTask():
 #
-# def some_job(counter):
-#     counter.inc()
-#     print('start', datetime.datetime.now())
-#     time.sleep(5)
-#     print('done', datetime.datetime.now())
+#     def my_listener(self, mevent):
+#         #print(str(mevent))
+#         print('listner: ', datetime.datetime.now(), str(self.counter.get()))
+#         self.counter.inc()
+#         if(self.counter.get() >= self.maxIterations):
+#             print('calling shutdown: ', str(datetime.datetime.now()))
+#             self.sched.shutdown()
 #
+#     def __init__(self, maxIterations):
+#         self.sched = BackgroundScheduler()
+#         self.counter = Counter()
+#         self.maxIterations = maxIterations
+#         self.sched.add_listener(self.my_listener, EVENT_JOB_EXECUTED)
 #
-# # seconds can be replaced with minutes, hours, or days
+#     def runTask(self, job, interval, args):
+#         self.sched.add_job(job, 'interval', seconds=interval, max_instances=100, args=args)
+#         self.sched.start()
 #
+#     def isDone(self):
+#         if self.counter.get() < self.maxIterations:
+#             print('not dont yet')
+#             return False
+#         else:
+#             print('done')
+#             return True
 #
-# counter = Counter()
+# # sc = ScheduledTask(40)
+# # sc.runTask(func, 1.5)
+# #
+# # while sc.isDone() == False:
+# #     time.sleep(1)
 #
-#
-# while counter.get() < max:
-#     time.sleep(1)
-
-
-#sched.shutdown()
-from apscheduler.schedulers.background import BackgroundScheduler
-
-
-class ScheduledTask():
-
-    def my_listener(self, mevent):
-        #print(str(mevent))
-        print('listner: ', datetime.datetime.now(), str(self.counter.get()))
-        self.counter.inc()
-        if(self.counter.get() >= self.maxIterations):
-            print('calling shutdown: ', str(datetime.datetime.now()))
-            self.sched.shutdown()
-
-    def __init__(self, maxIterations):
-        self.sched = BackgroundScheduler()
-        self.counter = Counter()
-        self.maxIterations = maxIterations
-        self.sched.add_listener(self.my_listener, EVENT_JOB_EXECUTED)
-
-    def runTask(self, job, interval, args):
-        self.sched.add_job(job, 'interval', seconds=interval, max_instances=100, args=args)
-        self.sched.start()
-
-    def isDone(self):
-        if self.counter.get() < self.maxIterations:
-            print('not dont yet')
-            return False
-        else:
-            print('done')
-            return True
-
-# sc = ScheduledTask(40)
-# sc.runTask(func, 1.5)
-#
-# while sc.isDone() == False:
-#     time.sleep(1)
-
